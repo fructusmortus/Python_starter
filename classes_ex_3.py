1c313745ac13013f6598ed58aa816de63501bb1b
fructusmortus <45312075+fructusmortus@users.noreply.github.com>
2021-01-20 21:43:26 +0600

    Add files via upload

---
+++ classes_ex_3.py
@@ -0,0 +1,15 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+	def __init__(self, name, max_speed, mileage):
+		super().__init__(name, max_speed, mileage)
+
+	def seating_capacity(self, capacity=50):
+		return super().seating_capacity(capacity=50)
